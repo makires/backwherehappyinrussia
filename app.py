7594fada038554adb4d0c8bf92f3c7acf252caf5
tilvutMac <fcognitive@gmail.com>
2023-01-14 14:07:19 +0300

    version for public

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
 from flask import Flask, jsonify, request, render_template, send_from_directory
-from flask_cors import CORS
+# from flask_cors import CORS
 from model.city_model import match_exact
 
-app = Flask(__name__, )
-CORS(app)
+app = Flask(__name__)
+# CORS(app)
 
-FLUTTER_WEB_APP = 'templates'
-
-
-@app.route('/web/')
-def render_page_web():
-    return render_template('index.html')
+# FLUTTER_WEB_APP = 'templates'
 
 
-@app.route('/web/<path:name>')
-def return_flutter_doc(name):
+# @app.route('/web/')
+# def render_page_web():
+#     return render_template('index.html')
 
-    datalist = str(name).split('/')
-    DIR_NAME = FLUTTER_WEB_APP
 
-    if len(datalist) > 1:
-        for i in range(0, len(datalist) - 1):
-            DIR_NAME += '/' + datalist[i]
+# @app.route('/web/<path:name>')
+# def return_flutter_doc(name):
 
-    return send_from_directory(DIR_NAME, datalist[-1])
+#     datalist = str(name).split('/')
+#     DIR_NAME = FLUTTER_WEB_APP
+
+#     if len(datalist) > 1:
+#         for i in range(0, len(datalist) - 1):
+#             DIR_NAME += '/' + datalist[i]
+
+#     return send_from_directory(DIR_NAME, datalist[-1])
 
 
 @app.route('/')
 def render_page():
-    return render_template('index.html')
+    return "WhereHappyInRussia"
+    # return render_template('index.html')
 
 
 @app.route("/search")
